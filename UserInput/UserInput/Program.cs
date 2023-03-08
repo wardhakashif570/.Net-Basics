127778d5a339b0120c7db79beaf6caa4352194e8
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-03-08 10:33:28 -0800

    How to take user input

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
+string firstname;
+string lasttname;
+Console.Write("Enter a first name: ");
+firstname = Console.ReadLine(); 
+Console.Write("Enter a last name: ");
+lasttname = Console.ReadLine();
+//Console.WriteLine("FirstName:" + firstname + ""+ "LastName" + lasttname);
+Console.WriteLine("Hello {0} {1}",firstname,lasttname);
+
+Console.ReadLine();
+
+//Console.WriteLine (Cursor on next Line and Console.Write cursor on same Line)
