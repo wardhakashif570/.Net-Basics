a7d5dedde01345d8551d24fc512ff088eb8c499d
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-03-09 09:41:53 -0800

    basic Controls Statements

---
+++ ControlStatement/ControlStatement/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = 10;
+if (number == 10)
+{
+    Console.WriteLine("The Number is 10");
+    Console.ReadLine();
+}
+else if (number == 11)
+{
+    Console.WriteLine("The Number is 11");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("The Number is not 10 nor 11");
+    Console.ReadLine();
+
+}
+
+// using the switch statement
+
+switch (number)
+{
+    case 10:
+        Console.WriteLine("The Number is 10");
+        break;
+    case 11:
+        Console.WriteLine("The Number is 11");
+        break;
+      default:
+        Console.WriteLine("The Number is not 10 nor 11");
+        break;
+ 
+}
+
