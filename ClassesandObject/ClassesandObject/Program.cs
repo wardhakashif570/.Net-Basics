f3ad94192b7bc95f93ee4a585e577dbda8f58d70
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-03-13 04:19:51 -0700

    Classes and Object

---
+++ ClassesandObject/ClassesandObject/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassesandObject;
+
+namespace ClassesandObject
+{
+    public class Class1
+    {
+        int number1 = 10;
+        int number2 = 20;
+
+        public  void Add(int number1, int number2)
+        {
+            Console.WriteLine("The sum of the number is "+ (number1 + number2));
+        }
+
+    }
+
+  
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Class1 obj1 = new Class1();
+        
+        obj1.Add(2, 4);
+    }
+}
