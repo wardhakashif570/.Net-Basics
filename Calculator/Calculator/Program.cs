86b3a3485de54ca78c9d35426c6a828783fd3848
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-03-13 10:20:25 -0700

    Calculator in C#

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Calculator
+{
+    int number1 = 10;
+    int number2 = 20;
+    int result;
+    public void Add()
+    {
+        result = number1 + number2;
+        ShowResult();
+    }
+    public void Subtract()
+    {
+        result = number1 - number2;
+        ShowResult();
+    }
+    public void Multiplication()
+    {
+        result = number1 * number2;
+        ShowResult();
+    }
+    public void  Devision()
+    {
+        result = number1 / number2;
+        ShowResult();
+    }
+    public void ShowResult()
+    {
+        Console.WriteLine(result);
+    }
+
+    public static void Main(string[] args)
+    {
+        Calculator obj = new Calculator();
+        obj.Subtract();
+
+    }
+}
