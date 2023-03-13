e599fa1d8ba0cb9f39f414cd4491bf591d785bbc
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-03-13 10:46:37 -0700

    Recursion in C#

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Recursion
+{
+    int number = 5;
+    int result = 1;
+    public int Fact()
+    {
+        if (number == 0)
+        {
+            return 1;
+        }
+
+        result = number * result;
+        number--;
+        Fact();
+
+        return result;
+
+    }
+
+}
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Recursion obj1 = new Recursion();
+       Console.WriteLine(obj1.Fact());
+    }
+}
