04cf34e9a3da0413adacd0503a4cb79e28b56e40
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-03-30 11:38:56 -0700

    Constructor in C#

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿public class ConstructorData
+{
+    string Firstname;
+    string Lastname;
+
+    public ConstructorData(string firstname, string lastname)
+    {
+        Firstname=firstname; Lastname=lastname;
+
+    }
+    public static void Main(string[] args)
+    {
+        ConstructorData obj1 = new ConstructorData("Wardha", "Kashif");
+        Console.WriteLine(obj1.Firstname);
+        Console.WriteLine(obj1.Lastname);
+        Console.ReadLine();
+    }
+}
