046c9fa11a2d2d6dd61a76d6b51d21b1bbe431fe
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-04-02 05:34:46 -0700

    Encapsulation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,34 @@
+﻿public class EncapsulationData
+{
+    private int accountNo = 10000;
+
+    /*public void SetData(int account)
+    {
+        accountNo = account;
+    }
+    public int GetAccountNo() { return accountNo; }*/
+
+    public int Balance
+    {
+        set
+        {
+            accountNo = value;
+        }
+        get
+        {
+            return accountNo;
+        }
+    }
+}
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EncapsulationData obj1 = new EncapsulationData();
+            obj1.Balance = 1000;
+            Console.WriteLine("Account Number is " + obj1.Balance);
+            Console.ReadLine();
+
+        }
+    }
