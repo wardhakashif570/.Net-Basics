b7b94c823a2adc3ed11cd316f8bcc8bf4eb59fcb
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-04-08 04:57:41 -0700

    Array in C#

---
+++ Array and List/Array and List/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Program
+{
+
+    static void Main(string[] args)
+
+    {
+        string[] cars = new string[4] { "BMW", "Corolla", "City", "Civic" };
+
+        {
+
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
