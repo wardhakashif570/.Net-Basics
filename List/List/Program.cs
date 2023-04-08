f14855aac407bf57b2d3c65f210dbf87ff62cd1c
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-04-08 05:23:47 -0700

    List in C#

---
+++ List/List/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> cars = new List<string>();
+        cars.Add("BMW");
+        cars.Add("Corolla");
+        cars.Add("Civic");
+
+        foreach(string car in cars)
+        {
+            Console.WriteLine(car);
+        }
+
+    }
+}
