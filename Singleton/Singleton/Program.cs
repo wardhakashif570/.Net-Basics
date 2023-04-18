5e18ac6207c9c4ee792dd5d650749bfe11055b1c
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-04-18 11:48:15 -0700

    Singleton in C#

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Singleton
+{
+    class Program
+    {
+        //sealed class restrict the Inheritance
+        static void Main(string[] args)
+        {
+            SingletonClass S1 = SingletonClass.GetInstance;
+            S1.PrintDetails("This is S1 Class");
+
+
+            SingletonClass S2 = SingletonClass.GetInstance;
+            S2.PrintDetails("This is S2 Class");
+        }
+
+    }
+}
