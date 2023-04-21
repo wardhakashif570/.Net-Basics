8e8404a8ffcfb58409b0e876444b5b5dfae91504
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-04-21 05:22:27 -0700

    Static Class

---
+++ StaticClass/StaticClass/Converter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    public static class Converter
+    {
+        public static double ToFahrenheit(double celcius)
+        {
+            return (celcius * 9 / 5) + 32;
+        }
+        public static double ToCelcius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+    }
+}
---
+++ StaticClass/StaticClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius = 37; double fahrenheit = 98.6;
+            Console.WriteLine("Value of {0} celcius to fahrenheit is {1}",
+                celcius, Converter.ToFahrenheit(celcius));
+            Console.WriteLine("Value of {0} fahrenheit to celcius is {1}",
+                fahrenheit, Converter.ToCelcius(fahrenheit));
+            Console.ReadLine();
+        }
+    }
+}
