19f5d9e60c0966174a55f10d3263762144210509
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-04-26 12:08:01 -0700

    Fibonacci Series

---
+++ FactoryDesignPattern/FactoryDesignPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace FactoryDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard cardDetails = CreditCardFactory.GetCreditCard("Platinum");
+
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType : " + cardDetails.GetCardType());
+                Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+                Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FibnonacciSeries/FibnonacciSeries/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int n1 = 0, n2 = 1, n3, num;
+
+        Console.WriteLine("Enter number of elements ");
+        num = int.Parse(Console.ReadLine());
+        Console.Write(n1 + " " + n2 + " ");
+        for (int i = 2; i <= num; i++)
+        {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+
+            Console.Write(n3 + " ");
+
+        }
+
+
+    }
+}
