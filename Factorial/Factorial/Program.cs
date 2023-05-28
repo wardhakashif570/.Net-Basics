1c281cfaaafeb251d1f1c9a55d7178a61933daec
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-05-28 10:20:34 -0700

    Factorial in C#

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number to Find Factorial");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int fact = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            fact = fact * i;
+        }
+
+        Console.WriteLine( "The factorial of a number is " + fact);
+    }
+}
