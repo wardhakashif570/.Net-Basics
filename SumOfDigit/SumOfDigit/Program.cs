67250d637cdd4d27c6bf509d70de5cf746156afd
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-05-29 10:18:38 -0700

    Sum of digit

---
+++ SumOfDigit/SumOfDigit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int sum = 0, m;
+        Console.WriteLine("Enter a number to calculate the sum of digits");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        while (number > 0)
+        {
+            m = number % 10;
+            sum = sum + m;
+            number = number / 10;
+        }
+        Console.WriteLine("The sum of a number is " + sum);
+    }
+}
