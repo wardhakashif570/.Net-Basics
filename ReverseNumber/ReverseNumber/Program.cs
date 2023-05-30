7009090fd1873769b98c2dd1b5885a1c485e81bb
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-05-30 10:33:12 -0700

    Reverse of a number

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number to reverse");
+        int input = Convert.ToInt32(Console.ReadLine());
+        int reverse = 0, temp;
+
+        while (input != 0)
+        {
+            temp = input % 10;
+            reverse = reverse * 10 + temp;
+            input = input / 10;
+        }
+
+        Console.WriteLine("The reverse of a number is " + reverse); 
+    }
+}
