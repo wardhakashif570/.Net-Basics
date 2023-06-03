be057781b16b30354f05b3edcb2416a243ac47ea
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-06-03 07:32:55 -0700

    DecimalToBinary

---
+++ DecimlToBinary/DecimlToBinary/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Decimal To Binary");
+        int DecimaNumber=Convert.ToInt32(Console.ReadLine());
+        string binaryNumber = Convert.ToString(DecimaNumber, 2);
+        Console.WriteLine(binaryNumber);
+    }
+}
