a9da32260d50220f3119f3700e0e015ee16b1dcc
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-06-03 06:36:41 -0700

    Swapping the numbers in C#

---
+++ swappingTheNumber/swappingTheNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //Swapping the two numbers
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int tempvar;
+        tempvar = number1;
+        number1 = number2;
+        number2 = tempvar;
+
+        Console.WriteLine("number1 " + number1);
+        Console.WriteLine("number2 " + number2);
+    }
+}
